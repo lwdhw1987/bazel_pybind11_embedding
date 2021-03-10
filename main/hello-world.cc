140f91a61d655b7ba83f2e00cfc8d5da59837c3e
william.pan <william.pan@enflame-tech.com>
2021-03-10 17:50:22 +0800

    Py_DecodeLocale Return a pointer to a newly allocated wide character string, use PyMem_RawFree() to free the memory. So there is no need to define wchar_t** _argv = PyMem_Malloc(sizeof(wchar_t*)*argc)

--- main/hello-world.cc
+++ main/hello-world.cc
@@ -16,7 +16,7 @@ int main(int argc, char** argv) {
 
   int py_argc = 9;
   py::scoped_interpreter guard{};
-  wchar_t** wargv = PyMem_New(wchar_t*, py_argc);
+  wchar_t* wargv[py_argc];
   std::string py_argv[py_argc] = {"convert.py",
                                   "--input",
                                   "resnet_v1_50_inference.pb",
@@ -29,21 +29,23 @@ int main(int argc, char** argv) {
   for (int i = 0; i < py_argc; i++) {
     wargv[i] = Py_DecodeLocale(py_argv[i].c_str(), nullptr);
     if (wargv[i] == nullptr) {
-      return EXIT_FAILURE;
+      fprintf(stderr, "Fatal error: cannot decode py_argv[%d]\n", i);
+      exit(1);
     }
   }
 
   PySys_SetArgv(py_argc, wargv);
 
   py::module_ sys = py::module_::import("sys");
-  // py::print(sys.attr("path"));
+  py::print(sys.attr("path"));
+  print_localtime();
   py::object convert = py::module_::import("tf2onnx.convert");
   py::object do_convert = convert.attr("main");
   do_convert();
   py::print(py::module::import("sys").attr("argv"));
 
   for (int i = 0; i < py_argc; i++) {
-    PyMem_Del(wargv[i]);
+    PyMem_RawFree(wargv[i]);
     wargv[i] = nullptr;
   }
   return 0;
